7799805e5152af68e245a7e28fe58d3e9271f444
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-05 20:32:03 -0600

    Add initialization for lengthOfLIS result list
    Initialize result list for storing lengths of increasing subsequences.

--- lengthOfLIS.py
+++ lengthOfLIS.py
@@ -5,3 +5,6 @@ from typing import List
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        res = [1] * len(nums)
+        for i in range(len(nums) - 1 , -1, -1):
+            for j in range(i + 1, len(nums)):
